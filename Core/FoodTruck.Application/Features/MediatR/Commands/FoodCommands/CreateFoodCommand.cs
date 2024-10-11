9e6335c71cb9720f5b6cbcf27a7bf12f22d21e23
erdem abat <erdemabat@hotmail.com>
2024-10-11 17:52:56 +0300

    createfood request updated

--- Core/FoodTruck.Application/Features/MediatR/Commands/FoodCommands/CreateFoodCommand.cs
+++ Core/FoodTruck.Application/Features/MediatR/Commands/FoodCommands/CreateFoodCommand.cs
@@ -11,8 +11,6 @@ namespace FoodTruck.Application.Features.MediateR.Commands.FoodCommands
         public string Description { get; set; }
         public int CountryId { get; set; }
         public int CategoryId { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? ImageLocalPath { get; set; }
         public IFormFile Image { get; set; }
         public List<int> FoodTasteIds { get; set; }
         public List<int> FoodMoodIds { get; set; }
